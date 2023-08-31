e28dcad302ad07e03534e668cb808beb1a98a2a6
Jamessora <cadaciojerico@gmail.com>
2023-08-31 10:31:20 +0800

    Updated cors for Frontend

--- frontend/XSwap-FE/src/apiConfig.js
+++ frontend/XSwap-FE/src/apiConfig.js
@@ -1,11 +1,10 @@
-// apiConfig.js
 let apiBaseURL;
 
 if (typeof window !== 'undefined') { // you're in the browser
     apiBaseURL = window.location.hostname === "localhost" ? 'http://localhost:3000' : 'https://xswap.onrender.com';
 } else {
     // you're in Node.js
-    apiBaseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+    apiBaseURL = 'http://localhost:3000'; // or any other way you want to get this value in a Node.js context
 }
 
 export default apiBaseURL;
