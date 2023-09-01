fecfa0ee6d26a294bbc192d58680fcc51dfe7793
Jamessora <cadaciojerico@gmail.com>
2023-09-01 20:29:51 +0800

    updated urls

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -6,7 +6,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
         if resource.errors.empty?
           set_flash_message!(:notice, :confirmed)
           # Redirect to your React app's URL where you want the user to go after confirming their account.
-          redirect_to 'http://localhost:5173/login'
+          redirect_to 'https://xswap-fe.onrender.com/login'
         else
           render :action => 'new'
         end
