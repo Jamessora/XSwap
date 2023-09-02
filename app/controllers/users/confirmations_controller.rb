19145262b84ddeb81b55df24929f08828843f316
Jamessora <cadaciojerico@gmail.com>
2023-09-02 19:05:18 +0800

    revised confirmations controller

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,24 +1,17 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
 
   def show
-      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-  
-      if resource.errors.empty?
-        set_flash_message!(:notice, :confirmed)
-        redirect_to "https://xswap-fe.onrender.com/confirm-email?confirmation_token=#{params[:confirmation_token]}"
-      else
-        render :action => 'new'
-      end
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      # Redirect to React frontend with success status
+      redirect_to "https://xswap-fe.onrender.com/confirmation-success?status=success"
+    else
+      # Redirect to React frontend with failure status
+      redirect_to "https://xswap-fe.onrender.com/confirmation-success?status=failure"
+    end
   end
 
-  def api_confirm
-      
-      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-      
-      if resource.errors.empty?
-        render json: { success: true, message: 'Email confirmed' }
-      else
-        render json: { success: false, errors: resource.errors.full_messages}, status: :unprocessable_entity
-      end
-  end
+  
 end
