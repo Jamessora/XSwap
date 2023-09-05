071f1e817101302c30adc8f39127dfb0146d0472
Jamessora <cadaciojerico@gmail.com>
2023-09-05 10:19:46 +0800

    changed api from coingecko to coincap.io

--- app/controllers/api/trades_controller.rb
+++ app/controllers/api/trades_controller.rb
@@ -157,55 +157,94 @@
             render json: { pnls: pnls }
         end
 
+        # def price
+        #     pair = params[:pair]
+        #     current_price = fetch_current_price(pair)
+        #     if current_price
+        #       render json: { price: current_price }, status: :ok
+        #     else
+        #       render json: { error: 'Could not fetch price' }, status: :internal_server_error
+        #     end
+        #   end
+
         def price
-            pair = params[:pair]
-            current_price = fetch_current_price(pair)
-            if current_price
-              render json: { price: current_price }, status: :ok
+            token_id = params[:pair]
+            token_data = fetch_token_data(token_id)
+          
+            if token_data
+              token = Token.find_or_create_by(external_id: token_data['id']) do |new_token|
+                new_token.ticker = token_data['symbol'].downcase
+                new_token.name = token_data['name']
+                new_token.price = token_data['priceUsd']
+              end
+          
+              # If the token was found, we update its data
+              if token.persisted?
+                token.update(
+                  ticker: token_data['symbol'].downcase,
+                  name: token_data['name'],
+                  price: token_data['priceUsd']
+                )
+              end
+          
+              render json: { status: 'success', data: token_data }, status: :ok
             else
-              render json: { error: 'Could not fetch price' }, status: :internal_server_error
+              render json: { status: 'error', message: 'Failed to fetch token data' }, status: :internal_server_error
             end
           end
-
+          
           private
 
-
-          def fetch_current_price(pair)
             require 'rest-client'
             require 'json'
-          
-            token = Token.find_or_create_by(ticker: pair) do |new_token|
-                new_token.name = pair.capitalize # or adjust this as necessary
-              end
-            
-              
 
-            if token.nil?
-            Rails.logger.error "Token not found for ticker: #{pair}"
-            return nil
-            end
-            
-            if token.price.nil? || token.price_updated_at < 1.minute.ago
-              begin
-                
-                response = RestClient.get "https://api.coingecko.com/api/v3/simple/price?ids=#{pair}&vs_currencies=usd"
-                json_data = JSON.parse(response.body)
-                current_price = json_data[pair]['usd']
-                
-                # Update the price in the database
-                token.update(price: current_price, price_updated_at: Time.now)
-              rescue RestClient::ExceptionWithResponse => e
-                Rails.logger.error "Error fetching price: #{e.response}"
-                nil
-              rescue => e
-                Rails.logger.error "An error occurred: #{e.message}"
-                nil
-              end
+            def fetch_token_data(token_id)
+            response = RestClient.get "https://api.coincap.io/v2/assets/#{token_id}?interval=d1"
+            data = JSON.parse(response.body)
+
+            if data['data']
+                return data['data']
             else
-              # Use the cached price
-              token.price
+                # handle error
+                return nil
             end
-          end
+            end
+        #         def fetch_current_price(pair)
+        #         require 'rest-client'
+        #         require 'json'
+            
+        #         token = Token.find_or_create_by(ticker: pair) do |new_token|
+        #             new_token.name = pair.capitalize # or adjust this as necessary
+        #         end
+                
+                
+
+        #         if token.nil?
+        #         Rails.logger.error "Token not found for ticker: #{pair}"
+        #         return nil
+        #         end
+                
+        #             if token.price.nil? || token.price_updated_at < 1.minute.ago
+        #             begin
+                        
+        #                 response = RestClient.get "https://api.coingecko.com/api/v3/simple/price?ids=#{pair}&vs_currencies=usd"
+        #                 json_data = JSON.parse(response.body)
+        #                 current_price = json_data[pair]['usd']
+                        
+        #                 # Update the price in the database
+        #                 token.update(price: current_price, price_updated_at: Time.now)
+        #             rescue RestClient::ExceptionWithResponse => e
+        #                 Rails.logger.error "Error fetching price: #{e.response}"
+        #                 nil
+        #             rescue => e
+        #                 Rails.logger.error "An error occurred: #{e.message}"
+        #                 nil
+        #             end
+        #             else
+        #             # Use the cached price
+        #             token.price
+        #         end
+        #   end
 
           
 
