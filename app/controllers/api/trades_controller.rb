5a275a86536cf7e04858377c09503bb412792aed
Jamessora <cadaciojerico@gmail.com>
2023-09-07 15:16:11 +0800

    Updated test controllers for the trades

--- app/controllers/api/trades_controller.rb
+++ app/controllers/api/trades_controller.rb
@@ -1,231 +1,185 @@
-    class Api::TradesController < ApplicationController
-        before_action :authenticate_user!
+class Api::TradesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_token, only: [:buy, :sell, :price]
+    before_action :set_transaction_params, only: [:buy, :sell]
+    
+    def buy
+        if @total_usd_value > current_user.balance
+          render json: { success: false, message: 'Insufficient balance' }, status: :unprocessable_entity
+          return
+        end
+    
+        create_transaction('buy')
+        update_portfolio_item(@amount)
+      end
+      
+    
+    def sell
+        portfolio_item = PortfolioItem.find_by(user: current_user, token: @token)
 
+        puts "Initial portfolio item amount: #{portfolio_item&.amount || 'Portfolio item not found'}"
+        if portfolio_item.nil? || portfolio_item.amount < @amount.to_f
+          render json: { success: false, message: 'Insufficient tokens', errors: ["You don't have enough #{@token_ticker} tokens"] }, status: :unprocessable_entity
+          return
+        end
+    
+        create_transaction('sell')
+        update_portfolio_item(-@amount)
+      end
+  
 
-        def buy
-            token_ticker = params[:token]
-            amount = params[:amount]
-            total_usd_value = params[:total_usd_value].to_f
-            
-            if total_usd_value > current_user.balance
-                render json: { success: false, message: 'Insufficient balance' }, status: :unprocessable_entity
-                return
-              end
+    def calculate_pnl
+        @transactions = Transaction.where(user_id: current_user.id)
+        pnls = [] # Hash to store the PNL for each token
+    
+        # Get unique token ids
+        unique_tokens = @transactions.pluck(:token_id).uniq
+    
+        unique_tokens.each do |token_id|
+        # Filter transactions for the current token
+        token_transactions = @transactions.where(token_id: token_id)
+        
+        puts "Calculated PNLs: #{pnls.inspect}"
 
-            per_token_price = fetch_current_price_new(token_ticker)
-            puts "Creating transaction with params: #{params.inspect}"
-            puts "Found Token: #{Token.find_by_ticker(token_ticker).inspect}"
-
-            
-                token = Token.find_or_create_by(ticker: token_ticker) do |new_token|
-                new_token.name = token_ticker.capitalize
-                
-            end
-
-            token.update(price: per_token_price)
-            
-
-            transaction = Transaction.create(
-                transaction_type: 'buy',
-                transaction_amount: amount,
-                transaction_price: per_token_price,
-                usd_value: total_usd_value,
-                user: current_user,
-                token: token
-            )
-            
-            puts "Transaction errors: #{transaction.errors.full_messages}" if transaction.errors.any?
-
-            if transaction.persisted?
-                current_user.update(balance: current_user.balance - total_usd_value)
-
-                portfolio_item = PortfolioItem.find_or_initialize_by(user: current_user, token: token)
-
-                # Update the amount
-                if portfolio_item.amount.nil?
-                portfolio_item.amount = 0
-                end
-                portfolio_item.amount += amount # 'amount' is the amount being bought
-
-                # Save the portfolio item
-                portfolio_item.save
-
-
-                render json: { success: true, message: 'Buy successful', transaction: transaction, user: { balance: current_user.balance } }, status: :created
-            else
-                render json: { success: false, message: 'Buy failed', errors: transaction.errors.full_messages }, status: :unprocessable_entity
+        remaining_usd_value = 0
+        remaining_amount_of_token = 0
+    
+        token_transactions.each do |transaction|
+            if transaction.transaction_type == "buy"
+            remaining_usd_value += transaction.transaction_amount * transaction.transaction_price
+            remaining_amount_of_token += transaction.transaction_amount
+            else # "sell"
+            remaining_usd_value -= transaction.transaction_amount * transaction.transaction_price
+            remaining_amount_of_token -= transaction.transaction_amount
             end
         end
-
-        def sell
-            token_ticker = params[:token]
-            amount = params[:amount]
-            total_usd_value = params[:total_usd_value].to_f  
-            
-            per_token_price = fetch_current_price_new(token_ticker)
-            puts "Creating transaction with params: #{params.inspect}"
-            puts "Found Token: #{Token.find_by_ticker(token_ticker).inspect}"
-
-            
-            token = Token.find_by(ticker: token_ticker)
-            
-            
-            if token.nil?
-                render json: { success: false, message: 'Token not found', errors: ["Token must exist"] }, status: :unprocessable_entity
-                return
-            end
-
-            portfolio_item = PortfolioItem.find_by(user: current_user, token: token)
-
-            # Check if the user has the token in their portfolio
-            if portfolio_item.nil?
-                render json: { success: false, message: 'Token not found in your portfolio', errors: ["You don't have enough #{token_ticker} tokens"] }, status: :unprocessable_entity
-                return
-            end
-            
-            # Check if the user has enough tokens to sell
-            if portfolio_item.amount < amount
-                render json: { success: false, message: 'Not enough tokens', errors: ["You don't have enough #{token_ticker} tokens"] }, status: :unprocessable_entity
-                return
-            end
-
-        # Create transaction
-            transaction = Transaction.create(
-                transaction_type: 'sell',
-                transaction_amount: amount,
-                transaction_price: per_token_price,
-                usd_value: total_usd_value,
-                user: current_user,
-                token: token
-            )
-
-            
-            puts "Transaction errors: #{transaction.errors.full_messages}" if transaction.errors.any?
-
-            if transaction.persisted?
-                current_user.update(balance: current_user.balance + total_usd_value)
-
-                # Update the amount if the portfolio item exists
-                if portfolio_item
-                portfolio_item.amount -= amount # 'amount' is the amount being sold
-                portfolio_item.save
-                end
-            
-                render json: { success: true, message: 'Sell successful', transaction: transaction, user: { balance: current_user.balance } }, status: :created
-            else
-                render json: { success: false, message: 'Sell failed', errors: transaction.errors.full_messages }, status: :unprocessable_entity
-            end
+    
+        # Fetch the current price
+        token = Token.find(token_id)
+        current_price = token.price
+        current_usd_value = remaining_amount_of_token * current_price
+    
+        pnl = current_usd_value - remaining_usd_value
+        
+        pnls.push({ token: token.name, pnl: pnl })
         end
-
-        def calculate_pnl
-            @transactions = Transaction.where(user_id: current_user.id)
-            pnls = [] # Hash to store the PNL for each token
-        
-            # Get unique token ids
-            unique_tokens = @transactions.pluck(:token_id).uniq
-        
-            unique_tokens.each do |token_id|
-            # Filter transactions for the current token
-            token_transactions = @transactions.where(token_id: token_id)
-            
-            puts "Calculated PNLs: #{pnls.inspect}"
-
-            remaining_usd_value = 0
-            remaining_amount_of_token = 0
-        
-            token_transactions.each do |transaction|
-                if transaction.transaction_type == "buy"
-                remaining_usd_value += transaction.transaction_amount * transaction.transaction_price
-                remaining_amount_of_token += transaction.transaction_amount
-                else # Assuming "sell"
-                remaining_usd_value -= transaction.transaction_amount * transaction.transaction_price
-                remaining_amount_of_token -= transaction.transaction_amount
-                end
-            end
-        
-            # Fetch the current price
-            token = Token.find(token_id)
-            current_price = token.price
-            current_usd_value = remaining_amount_of_token * current_price
-        
-            pnl = current_usd_value - remaining_usd_value
-            
-            pnls.push({ token: token.name, pnl: pnl })
-            end
-        
-            # pnls now contains the PNL for each token
-            render json: { pnls: pnls }
-        end
-
-       
-
-        def price
-            token_id = params[:pair]
-            token_data = fetch_token_data(token_id)
-          
-            if token_data
-              token = Token.find_or_create_by(external_id: token_data['id']) do |new_token|
-                new_token.ticker = token_data['symbol'].downcase
-                new_token.name = token_data['name']
-                new_token.price = token_data['priceUsd']
-              end
-          
-              # If the token was found, we update its data
-              if token.persisted?
-                token.update(
-                  ticker: token_data['symbol'].downcase,
-                  name: token_data['name'],
-                  price: token_data['priceUsd']
-                )
-              end
-          
-              render json: { status: 'success', data: token_data }, status: :ok
-            else
-              render json: { status: 'error', message: 'Failed to fetch token data' }, status: :internal_server_error
-            end
-          end
-
-        
-
-          private
-
-            
-            def fetch_current_price_new(token_id)
-            require 'rest-client'
-            require 'json'
-          
-            response = RestClient.get "https://api.coincap.io/v2/assets/#{token_id}?interval=d1"
-            data = JSON.parse(response.body)
-          
-            if data['data']
-                return data['data']['priceUsd'].to_f
-            else
-                # handle error
-                return nil
-            end
-            end
-
-            def fetch_token_data(token_id)
-            require 'rest-client'
-            require 'json'
-
-            response = RestClient.get "https://api.coincap.io/v2/assets/#{token_id}?interval=d1"
-            data = JSON.parse(response.body)
-
-            if data['data']
-                return data['data']
-            else
-                # handle error
-                
-            return nil
-            end
-                rescue RestClient::ExceptionWithResponse => e
-                Rails.logger.error "Error fetching data from API: #{e.response}"
-                return nil
-            
-           end
-        #   
-
-          
-
+    
+        # pnls now contains the PNL for each token
+        render json: { pnls: pnls }
     end
+
+   
+
+    def price
+        token_id = params[:pair]
+        token_data = fetch_token_data(token_id)
+      
+        if token_data
+          token = Token.find_or_create_by(external_id: token_data['id']) do |new_token|
+            new_token.ticker = token_data['symbol'].downcase
+            new_token.name = token_data['name']
+            new_token.price = token_data['priceUsd']
+          end
+      
+          # If the token was found, we update its data
+          if token.persisted?
+            token.update(
+              ticker: token_data['symbol'].downcase,
+              name: token_data['name'],
+              price: token_data['priceUsd']
+            )
+          end
+      
+          render json: { status: 'success', data: token_data }, status: :ok
+        else
+          render json: { status: 'error', message: 'Failed to fetch token data' }, status: :internal_server_error
+        end
+      end
+
+    
+
+      private
+
+      def set_token
+        @token_ticker = params[:token]
+        @token = Token.find_or_initialize_by(ticker: @token_ticker) do |new_token|
+          new_token.name = @token_ticker.capitalize
+        end
+        @token.update(price: fetch_current_price_new(@token_ticker))
+      end
+
+      def set_transaction_params
+        @amount = params[:amount].to_f
+        @total_usd_value = params[:total_usd_value].to_f
+      end
+
+
+      def create_transaction(type)
+        transaction = Transaction.create(
+          transaction_type: type,
+          transaction_amount: @amount,
+          transaction_price: @token.price,
+          usd_value: @total_usd_value,
+          user: current_user,
+          token: @token
+        )
+        
+        if transaction.persisted?
+          current_user.update(balance: current_user.balance + (@total_usd_value * (type == 'sell' ? 1 : -1)))
+          render json: { success: true, message: "#{type.capitalize} successful", transaction: transaction, user: { balance: current_user.balance } }, status: :created
+        else
+          render json: { success: false, message: "#{type.capitalize} failed", errors: transaction.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update_portfolio_item(amount_change)
+        portfolio_item = PortfolioItem.find_or_initialize_by(user: current_user, token: @token)
+        
+        puts "Before update, portfolio_item amount: #{portfolio_item.amount || 0}"
+        puts "Amount change: #{amount_change}"
+
+        portfolio_item.amount = (portfolio_item.amount || 0) + amount_change.to_f
+        portfolio_item.save
+
+        puts "After update, portfolio_item amount: #{portfolio_item.amount}"
+      end
+        
+        def fetch_current_price_new(token_id)
+        require 'rest-client'
+        require 'json'
+      
+        response = RestClient.get "https://api.coincap.io/v2/assets/#{token_id}?interval=d1"
+        data = JSON.parse(response.body)
+      
+        if data['data']
+            return data['data']['priceUsd'].to_f
+        else
+            # handle error
+            return nil
+        end
+        end
+
+        def fetch_token_data(token_id)
+        require 'rest-client'
+        require 'json'
+
+        response = RestClient.get "https://api.coincap.io/v2/assets/#{token_id}?interval=d1"
+        data = JSON.parse(response.body)
+
+        if data['data']
+            return data['data']
+        else
+            # handle error
+            
+        return nil
+        end
+            rescue RestClient::ExceptionWithResponse => e
+            Rails.logger.error "Error fetching data from API: #{e.response}"
+            return nil
+        
+       end
+    #   
+
+      
+
+end
--- spec/controllers/trades_controller_spec.rb
+++ spec/controllers/trades_controller_spec.rb
@@ -1,69 +1,63 @@
 require 'rails_helper'
 require 'webmock/rspec'
 
-    RSpec.describe Api::TradesController, type: :controller do
-    include Devise::Test::ControllerHelpers
+RSpec.describe Api::TradesController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
-    before :each do
-        @request.env["devise.mapping"] = Devise.mappings[:user]
-        @user = create(:user, :confirmed, :with_kyc)
-        @token = create(:token, id:'bitcoin', ticker: 'BTC', name: 'Bitcoin', price: 27000.0)
-        sign_in @user
+  before :each do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = create(:user, :confirmed, :with_kyc, balance: 1000.0, kyc_status: nil)
+    @token = create(:token, ticker: 'BTC', name: 'Bitcoin', price: 27000.0)
+    sign_in @user
 
-        # Stubbing the external request
-        WebMock.stub_request(:get, /https:\/\/api\.coincap\.io\/v2\/assets\/.*\?interval=d1/)
-            .to_return(
-                status: 200,
-                body: '{"data": {"priceUsd": "27000.0"}}',
-                headers: {}
-            )
+    WebMock.stub_request(:get, "https://api.coincap.io/v2/assets/#{@token.ticker}?interval=d1")
+      .to_return(
+        status: 200, 
+        body: '{"data": {"priceUsd": "27000.0"}}', 
+        headers: {}
+      )
+  end
+  # ... (test cases will go here)
+  describe "POST #buy" do
+    context "with insufficient balance" do
+      it "returns unprocessable entity status" do
+        post :buy, params: { token: 'BTC', amount: 1.0, total_usd_value: 27001.0 }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)["message"]).to eq('Insufficient balance')
+      end
     end
-
-    
-
-    describe "POST #buy" do
-        context "with insufficient balance" do
-          it "returns unprocessable entity status" do
-            post :buy, params: { token: 'BTC', amount: 1.0, total_usd_value: 27001.0 }
-            expect(response).to have_http_status(:unprocessable_entity)
-            expect(JSON.parse(response.body)["message"]).to eq('Insufficient balance')
-          end
-        end
-    
-        context "with sufficient balance and valid params" do
-          it "creates a new buy transaction and updates user balance and portfolio" do
-            post :buy, params: { token: 'BTC', amount: 0.01, total_usd_value: 27000.0 }
-            expect(response).to have_http_status(:created)
-            expect(@user.reload.balance).to eq(1000.0 - 270.0)
-            expect(PortfolioItem.find_by(user: @user, token: @token).amount).to eq(0.01)
-          end
-        end
+  
+    context "with sufficient balance and valid params" do
+      it "creates a new buy transaction and updates user balance and portfolio" do
+        post :buy, params: { token: 'BTC', amount: 0.01, total_usd_value: 270.0 }
+        expect(response).to have_http_status(:created)
+        expect(@user.reload.balance).to eq(730.0)
+        expect(PortfolioItem.find_by(user: @user, token: @token).amount).to eq(0.01)
       end
-    
-      describe "POST #sell" do
-        context "with insufficient tokens" do
-          it "returns unprocessable entity status" do
-            post :sell, params: { token: 'BTC', amount: 1.0, total_usd_value: 27000.0 }
-            expect(response).to have_http_status(:unprocessable_entity)
-            expect(JSON.parse(response.body)["message"]).to eq('Token not found in your portfolio')
-          end
-        end
-    
-        context "with sufficient tokens and valid params" do
-          before do
-            create(:portfolio_item, user: @user, token: @token, amount: 1.0)
-          end
-    
-          it "creates a new sell transaction and updates user balance and portfolio" do
-            post :sell, params: { token: 'BTC', amount: 0.5, total_usd_value: 13500.0 }
-            expect(response).to have_http_status(:created)
-            expect(@user.reload.balance).to eq(1000.0 + 13500.0)
-            expect(PortfolioItem.find_by(user: @user, token: @token).amount).to eq(0.5)
-          end
-        end
+    end
+  end
+
+
+  describe "POST #sell" do
+    context "with insufficient tokens" do
+      it "returns unprocessable entity status" do
+        post :sell, params: { token: 'BTC', amount: 1.0, total_usd_value: 27000.0 }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)["message"]).to eq('Insufficient tokens')
       end
-    
-
-
-    
+    end
+  
+    context "with sufficient tokens and valid params" do
+      before do
+        create(:portfolio_item, user: @user, token: @token, amount: 1.0)
+      end
+      it "creates a new sell transaction and updates user balance and portfolio" do
+         expect(PortfolioItem.find_by(user: @user, token: @token).amount).to eq(1.0)
+        post :sell, params: { token: 'BTC', amount: 0.01, total_usd_value: 270.0 }
+        expect(response).to have_http_status(:created)
+        expect(@user.reload.balance).to eq(1270.0) # Assuming user initially had 1000 USD
+        expect(PortfolioItem.find_by(user: @user, token: @token).amount).to eq(0.99) # Assuming user initially had 1 BTC
+      end
+    end
+  end
 end
